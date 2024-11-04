b3b6fde48b2a9c3ef977832421c3981720cc07e6
SunnyKumar-code <sk202542@gmail.com>
2024-11-04 08:37:59 +0530

    remove

--- app.js
+++ app.js
@@ -5,23 +5,17 @@ function updateClock() {
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
     const amPm = hours >= 12 ? 'pm' : 'am';
-  
     // Convert 24-hour time to 12-hour time
     hours = hours % 12;
     hours = hours===0 ? 12 : hours; // the hour '0' should be '12'
-  
     // Pad single digits with leading zeros
     const hourStr = String(hours).padStart(2, '0');
     const minuteStr = String(minutes).padStart(2, '0');
     const secondStr = String(seconds).padStart(2, '0');
-  
-
     document.getElementById('hour-hand').textContent = hourStr;
     document.getElementById('minute-hand').textContent = minuteStr;
     document.getElementById('second-hand').textContent = secondStr;
     document.getElementById('am-pm').textContent = amPm;
-  
-    
     changeBackgroundImage(now.getHours());
   }
   
